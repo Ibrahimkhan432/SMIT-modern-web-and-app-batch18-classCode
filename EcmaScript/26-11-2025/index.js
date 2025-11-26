84de1864ddd5f9c80055f9760ce87f989931a524
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-11-26 11:13:47 +0500

    prompt, if else

---
+++ EcmaScript/26-11-2025/index.js
@@ -0,0 +1,100 @@
+// var a = 2
+
+// var decre = a-- + a-- + --a
+// // console.log(a--)
+// console.log(decre)
+
+// var a = 1
+// var b = 1
+// var result =  a++ +  b++ + a++ + ++a +  ++b
+// //             2. +. 1   +2   + 4   +2
+// console.log(result)
+
+// var num = ((3 * 4) + 2 )* 5
+// console.log(num)
+
+// var mymessage = "welcome you "
+
+// var name = "ali"
+
+// console.log(mymessage + name + " khan")
+// console.log(mymessage + name + " khan")
+// console.log(mymessage + name + " khan")
+
+// var num =  3 + 3 + "2" + 5+ 6+ 6
+// console.log(num)
+
+// alert (just for message)
+// propmt("message with user information")
+
+// prompt("hi","ibrahim")
+// var userName = "ibrahim"
+// var message = "you are handsome"
+// var result = prompt(userName)
+
+// var number1 = Number(prompt("enter first number"));
+// var number2 = +prompt("enter second number");
+
+// var result = number1 + number2;
+
+// console.log(result);
+
+// var dogs = prompt("how many dogs in your area")
+// var calculate = dogs + 3
+// console.log( "calculate=>(44)",calculate)
+
+// var prompt = require('prompt-sync')();
+
+// var number = +prompt("enter a number ")
+// var sum = number + 1
+// console.log("sum=>", sum)
+
+//   --- if else
+
+// if (true) {
+//   console.log("cond is sahi");
+// }
+// console.log("outside cond");
+
+// var uname = prompt("enter your name");
+// var absentStudent = prompt("enter absent student")
+
+// if (uname === absentStudent) {
+//   console.log(uname + " you are not allow");
+// } else {
+//   console.log( uname +" you are allow");
+// }
+// "2" == 2 true
+// "2" === 2
+// = assign
+// == condition
+// === condition with type(string,number)
+
+//   --comparision
+// var num1 = 2
+// var num2 = 2
+// if(num2 === num1 ){
+//     console.log("you are right")
+// }
+
+// var uname = prompt("enter user name");
+
+// if (uname === "ali") {
+//   console.log("you were absent");
+// } else if (uname === "asad") {
+//   console.log("you were absent");
+// } else if (uname === "bilal") {
+//   console.log("you were absent");
+// } else {
+//   console.log("you were present");
+// }
+
+var stdname = prompt("enter your name");
+var stdage = Number(prompt("enter your age"));
+
+var name = "ali";
+// var age = 16;
+
+if (stdname === name && stdage > 20 ) {
+  console.log("you passed matric");
+}
