45036a9cdb164f691e7af0f25efaace135ecb557
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-12-01 11:05:33 +0500

    data types in array &  for loop intro

---
+++ EcmaScript/1-12-2025/index.js
@@ -0,0 +1,79 @@
+// console.log("js is running");
+// var arr = ["cat", "dog", "snake", "a", "b", "c"];
+
+// var newArr = arr.slice(0, 1); //0 , 1 (1-0 = 1)
+// var newArr = arr.slice(2, 5); //4 , 2 (4-2 = 2)
+
+// var newArr = arr.slice(0,3);
+// console.log("slice", newArr);
+
+// var newArr = arr.splice(0,1);
+// console.log("new arr=>", newArr);
+// console.log("arr=>", arr);
+
+// var arr = []
+// arr[300] = "a"
+// console.log(arr.length)
+
+// push //fast
+// unshift //slow
+// pop //fast
+// shift //slow
+
+// console.log([]==[])
+
+// console.log(" "==" ")
+// if(" "){
+//     console.log("in if")
+// }
+// var numer = "3";
+// var arr1 = [];
+// console.log("arr1", arr1);
+
+// var arr2 = new Array(10);
+// console.log("arr2", arr2);
+
+// var name = "ibrahim"
+
+//    name = 3443243
+
+// var newname = name
+// newname = "khan"
+// console.log(name)
+// console.log(newname)
+
+// var arr = ["ibrahim"]
+// var newArr = arr
+
+// var newArr = [...arr]
+
+// newArr = ["khan"]
+// console.log(arr)
+// console.log(newArr)
+
+// var i = 0;
+// i > 10;
+// i++;
+
+// for (var i = 1 ; i < 11; i++) {  // ICU
+//   console.log("2 " + " x " + i + " = " + (i * 2));
+// }
+//  0.  1.  2. 3.   4
+// var arr = ["a","b","c","d","e","b","c","d","e"]
+
+// for (let i = 0; i < arr.length; i++) {
+// console.log(arr[i])
+// }
+
+// var stds = ["ali", "bilal", "asad", "saad", "anas"];
+// var absentStnd = prompt("enter absent stdnt name");
+// for (let i = 0; i < stds.length; i++) {
+//   if (absentStnd == stds[i]) {
+//     console.log("you are absent", stds[i]);
+//     break;
+//   } else {
+//     console.log("not found");
+//   }
+//  }
+
+//  print 2 table in reverse
