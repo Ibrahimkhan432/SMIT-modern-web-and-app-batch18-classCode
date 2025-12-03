cad96041f3007ce84f21b8914dbb57605b0709df
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-12-03 11:11:23 +0500

    2d array with nested for loop

---
+++ EcmaScript/3-12-2025 /index.js
@@ -0,0 +1,36 @@
+// console.log("js is alive")
+
+// var arr =["a","b",3,4]
+// console.log(arr.length)
+// console.log(arr.length-1)
+
+// console.log("array last element",arr[arr.length-1])
+
+// var twoDArray = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9,[2,"g",["c",10]]],
+// ];
+// console.log("twodArray",twoDArray)
+// console.log("length",twoDArray.length)
+// console.log(twoDArray[2][3][2][0])
+
+// assignment  // add each element of  these arrays
+// var arr1 =[[1,2,3],[4,5,6],[7,8,9]]
+// var arr2 =[[9,8,7],[6,5,4],[3,2,1]]
+
+// var sumOfArr =[]
+// // arr1[0][0] + arr2[2][2]
+// //    1 +
+
+// var arr =[1,2,3,4,5]
+// var twoDArr =[[1,2,3],[4,5,6],[7,8,9]]
+// console.log(twoDArr[0]) //[1,2,3]
+// for(var i=0; i<twoDArr.length; i++){ //iteration
+//    for (let j = 0; j < twoDArr[i].length; j++) {
+//        console.log(twoDArr[i][j])
+//    }
+// }
+
+// ASSIGNEMT PRACTICE
+// use flag in  for and if condition
