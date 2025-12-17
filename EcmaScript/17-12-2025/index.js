871717e634927dbd1dde2b65d54526fbcbb2f1ea
Ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2025-12-17 16:11:48 +0500

    Revision Chap 1 to 5

---
+++ EcmaScript/17-12-2025/index.js
@@ -0,0 +1,53 @@
+//  BOM browser object model
+// Read only (Ok)
+// alert("hi alert")
+
+// Variables
+// vary(change) ables(able)
+// var uname = {
+//   names: "ibrahim",
+//   roll: 30,
+//   age: 18,
+// };
+// var myName = "ibrahim";  //assign
+// var myName ="khan"
+// const lastName = "khan";
+
+// console.warn(myName);
+// console.error(myName);
+// console.table(uname);
+
+// var test;
+// test ="tofy"
+// console.log(test)
+
+// var empty = null
+// console.log(empty)
+// console.log(myFirstName)
+let myFirstName = "ibrahim"  //reserved
+myFirstName //reinitialized
+myFirstName = "ibrahim change" //reassign
+
+
+// let cannot access(TDZ = temporal dead zone)
+
+// variable name convention
+// pascal case 
+// camel case (easy to read and write)
+// snake case 
+// train case
+
+// var number = 10
+// var number = "10"
+
+// var numberPlus = number - 3 //(temporary) //coersion
+// console.log("number",number,numberPlus)
+
+// var myName
+// var $name
+// var _name
+// var 007khan
+// var 007champion
+
+var modulus = 10 /0
+console.log(modulus)
