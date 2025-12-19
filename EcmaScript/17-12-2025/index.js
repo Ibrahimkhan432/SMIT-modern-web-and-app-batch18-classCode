28d1eedfe9207d69b12a9f321d930b9a1d3315ea
Ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2025-12-19 10:56:48 +0500

    Revision chap 6 to 15

--- EcmaScript/17-12-2025/index.js
+++ EcmaScript/17-12-2025/index.js
@@ -1,53 +1,76 @@
-//  BOM browser object model
-// Read only (Ok)
-// alert("hi alert")
+// ++a pre increment
+// a++ post increment
 
-// Variables
-// vary(change) ables(able)
-// var uname = {
-//   names: "ibrahim",
-//   roll: 30,
-//   age: 18,
-// };
-// var myName = "ibrahim";  //assign
-// var myName ="khan"
-// const lastName = "khan";
+// var num = 2;
 
-// console.warn(myName);
-// console.error(myName);
-// console.table(uname);
+// var num2 = num + 1;
+// var num2 = num++; //shortcut
 
-// var test;
-// test ="tofy"
-// console.log(test)
+// // var num2 = --num;
 
-// var empty = null
-// console.log(empty)
-// console.log(myFirstName)
-let myFirstName = "ibrahim"  //reserved
-myFirstName //reinitialized
-myFirstName = "ibrahim change" //reassign
+// console.log(num2)
+// console.log(num)
 
+var a = 1;
+var b = 1;
+//  2 + 1 + 2 +  2  3
+// var total = ++a + b++ + b + a++ + a
+// var total = ++b + ++b + ++a + ++a
+// var total = a++ + ++b + a + b++ + a + b
+// console.log(total) //10, 8 , 9 , 6
 
-// let cannot access(TDZ = temporal dead zone)
+// var num = (1 * 2) * (3 + 2);
+// console.log(num);
 
-// variable name convention
-// pascal case 
-// camel case (easy to read and write)
-// snake case 
-// train case
+// var mname = "ibrahim"
 
-// var number = 10
-// var number = "10"
+// var lname = prompt("enter your lname")
 
-// var numberPlus = number - 3 //(temporary) //coersion
-// console.log("number",number,numberPlus)
+// alert( "Hi " +mname + lname)
+// console.log("hi"  + " " + mname + " " + lname)
 
-// var myName
-// var $name
-// var _name
-// var 007khan
-// var 007champion
+// console.log(`Hi ${mname} ${lname} how are you`)
+// template literals ` `
 
-var modulus = 10 /0
-console.log(modulus)
+// var age = prompt("enter your age"30)
+// console.log(age)
+// study assignment (why give null when cancel)
+// prompt("enter")
+// var mname = prompt("enter your name");
+// var age = +prompt("enter your age");
+// var marks = Number(prompt("enter your marks"));
+// if (mname == "ali" && age !== 30 || age < 30 && marks <=100) {
+//   console.log("correct", mname , age , marks);
+// } else if (mname == "bilal") {
+//   console.log("correct", mname);
+// } else {
+//   console.log("incorrect");
+// }
+
+//   -- nested if
+// diff b/w. if else if and nested if 
+// if else  (if filed then else will also check)
+// nested if( if failed then next if will not be check)
+// means the second if will check after first if true
+// var mname = "li"
+// var age = 10
+
+// if(mname=="ali"){
+//     if(age==10)
+//     console.log("true name and age")
+// }
+
+//  --Array 
+
+// var arr = [ , ,20,"ali",100]
+// console.table(arr)
+// console.log(arr[2])
+
+var arr =[]
+
+arr[200] = "ali" //length 201 (200 index)
+arr[150] = "asad" // 151
+console.log(arr[arr.length-1])
+
+console.log(arr[arr.length-51])
+console.log(arr.length)
