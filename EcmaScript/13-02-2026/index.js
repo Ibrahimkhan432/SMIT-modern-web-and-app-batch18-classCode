f4f40c2343f255b3bbad85257dd31677919d5b48
Ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2026-02-13 10:56:16 +0500

    Es6 - callback functions

---
+++ EcmaScript/13-02-2026/index.js
@@ -0,0 +1,41 @@
+// functions 
+// callback functions
+
+// const funct = function myFunc(){
+//     console.log("inside fuction")
+// }
+// funct()// declaration //expression
+
+// function calculate(a,b){
+//    console.log(a+b)
+// }
+// calculate(2,3)
+// es6
+// const arrowFunction = (a,b) => console.log(a+b)
+// arrowFunction(4,5)
+// var num1 = +prompt("enter num1")
+// var num2 = +prompt("enter num2")
+// var obj = {
+//     // a:3,
+//     b:6,
+
+// }
+
+// console.log(obj.b?.c) //optional chaining
+// console.log(obj.a ? obj.a : obj.b ?  "nhi he")
+// const  {a,b } = obj //destructure
+// function calculate(a,b,callback){
+//   return callback(a,b)
+// }
+// function sum(a,b){
+//   return a+b
+// }
+// function minus(a,b){
+//   return a-b
+// }
+// console.log(calculate(obj.a,obj.b,sum))
+
+// function delay(){
+//     console.log("i will console lately")
+// }
+// setTimeout(delay ,3000)
