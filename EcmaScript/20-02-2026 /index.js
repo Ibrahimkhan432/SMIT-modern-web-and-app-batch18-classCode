99c07bb41d5aaec68759576b7cbb8dab5d8bb20c
Ibrahimkhan432 <ibrahimsheikh772@gmail.com>
2026-02-23 11:19:43 +0500

    firebase - signin, logout, cont with google

--- EcmaScript/20-02-2026 /index.js
+++ EcmaScript/20-02-2026 /index.js
@@ -1,13 +1,20 @@
 // 1.type module in script
 // 2.live server
-
+// import dotenv from "dotenv";
+// dotenv.config();
+// console.log(process.env.API_KEY);
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import {
   getAuth,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup
 } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: "AIzaSyDKU1D-ZgLrmgTpAbgMgJtA4U4e70jnb8M",
   authDomain: "smit-mwa-batch-18.firebaseapp.com",
   projectId: "smit-mwa-batch-18",
   storageBucket: "smit-mwa-batch-18.firebasestorage.app",
@@ -23,10 +30,32 @@ console.log("initialized app=>", app);
 const auth = getAuth(app);
 console.log("auth=>", auth);
 
+var text = document.getElementById("text");
+
 //signup
 var signupBtn = document.getElementById("signupBtn");
 signupBtn.addEventListener("click", signup);
 
+//login
+var loginBtn = document.getElementById("loginBtn");
+loginBtn.addEventListener("click", login);
+
+// logout
+var logoutBtn = document.getElementById("logout");
+logoutBtn.addEventListener("click", logout);
+
+// continue with google
+var googleBtn = document.getElementById("google");
+googleBtn.addEventListener("click", google);
+
+// on reload data
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    text.innerText = user.email;
+  } else {
+  }
+});
+// signup
 function signup() {
   var semail = document.getElementById("semail").value;
   var spassword = document.getElementById("spassword").value;
@@ -35,11 +64,62 @@ function signup() {
     .then((userCredential) => {
       const user = userCredential.user;
       console.log("user=>", user);
+      text.innerText = user.email;
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      console.log("error",errorMessage)
-      console.log("errorcode",errorCode)
+      console.log("error", errorMessage);
+      console.log("errorcode", errorCode);
     });
+}
+
+function login() {
+  var lemail = document.getElementById("lemail").value;
+  var lpassword = document.getElementById("lpassword").value;
+
+  signInWithEmailAndPassword(auth, lemail, lpassword)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("user=>", user);
+      text.innerText = user.email;
+      text.style.color = "green";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("error", errorMessage);
+      console.log("errorcode", errorCode);
+      text.innerText = errorCode;
+      text.style.color = "red";
+    });
+}
+
+function logout() {
+  signOut(auth)
+    .then(() => {
+      console.log("signout successfully");
+      text.innerText = "";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+// cont with google
+const provider = new GoogleAuthProvider();
+function google(){
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    console.log("user google=>",user.providerData[0].displayName)
+    // text.innerText=user.providerData[0].displayName
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+  });
 }
